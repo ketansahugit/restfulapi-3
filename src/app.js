88f33ecc9c9e9191e8af5456d058653010a73817
Ketan Sahu <rorozoro9888@gmail.com>
2023-09-09 22:31:30 +0530

    post

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 require("./db/conn");
+const Student = require("./models/students");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("hello from the other sideo from ketan.");
 })
 
 app.post("/students", (req, res) => {
-    res.send("hello from the other side.");
+    const user = new Student(req.body);
+    user.save().then(() => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+    
 })
 
 app.listen(port, () => {
--- src/models/students.js
+++ src/models/students.js
@@ -6,5 +6,31 @@ const studentSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email id already present"],
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+    phone: {
+        type: Number,
+        min: 10,
+        max: 10,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
     }
 })
+
+// We will create a new collection
+const Student = new mongoose.model('Student', studentSchema);
+
+module.exports = Student;
