0fefad5f168879177de0393848d9480ee791bd93
Ketan Sahu <rorozoro9888@gmail.com>
2023-09-10 23:05:14 +0530

    get requests

--- src/app.js
+++ src/app.js
@@ -6,20 +6,55 @@ const Student = require("./models/students");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("hello from the other sideo from ketan.");
-})
+// read the data of registered Students
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+  } catch (e) {
+    res.send(e);
+  }
+});
 
-app.post("/students", (req, res) => {
-    const user = new Student(req.body);
-    user.save().then(() => {
-        res.send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
+// get the individual student data using id
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// create new students
+/* app.post("/students", (req, res) => {
+  const user = new Student(req.body);
+  user
+    .save()
+    .then(() => {
+      res.send(user);
     })
-    
-})
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+}); */
+
+// create new students - asynchronously
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`connection is setup at ${port}`);
-})
+  console.log(`connection is setup at ${port}`);
+});
--- src/models/students.js
+++ src/models/students.js
@@ -2,35 +2,34 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const studentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email id already present"],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
     },
-    email: {
-        type: String,
-        required: true,
-        unique: [true, "Email id already present"],
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Invalid Email")
-            }
-        }
-    },
-    phone: {
-        type: Number,
-        min: 10,
-        max: 10,
-        required: true,
-        unique: true
-    },
-    address: {
-        type: String,
-        required: true
-    }
-})
+  },
+  phone: {
+    type: Number,
+    min: 10,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
 
 // We will create a new collection
-const Student = new mongoose.model('Student', studentSchema);
+const Student = new mongoose.model("Student", studentSchema);
 
 module.exports = Student;
